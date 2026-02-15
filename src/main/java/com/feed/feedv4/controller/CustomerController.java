0cff7cc5b075f7ce65ea89adaeadafd0df967b23
Icescottea <mariousenesh@gmail.com>
2026-02-15 16:56:13 +0530

    fix 7

--- src/main/java/com/feed/feedv4/controller/CustomerController.java
+++ src/main/java/com/feed/feedv4/controller/CustomerController.java
@@ -1,17 +1,28 @@
 package com.feed.feedv4.controller;
 
-import com.feed.feedv4.dto.CustomerDTO;
-import com.feed.feedv4.service.CustomerService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import jakarta.validation.Valid;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.feed.feedv4.dto.CustomerDTO;
+import com.feed.feedv4.service.CustomerService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
 @RestController
-@RequestMapping("/api/customers")
+@RequestMapping("/customers")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class CustomerController {
